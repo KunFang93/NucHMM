88d7879e3abdddc5febf72dd9bdd82c7d1575c4e
KunFang <fangkun930601@me.com>
2022-02-21 09:53:41 -0600

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NucHMM',
-    version='1.0',
+    version='1.2',
     py_modules=['NucHMM'],
     install_requires=[
         'Click','scipy','numpy','matplotlib','pandas','seaborn','statsmodels'
